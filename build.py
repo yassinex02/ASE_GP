fdd9c8ca5dbfe096a4992986e3846012eca02129
Taha Yassine Moumni <yassine@Tahas-MacBook-Air.local>
2023-09-19 22:25:15 +0200

    files adding

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def function():
+    df = pd.read_csv('data/dataset.csv', delimiter=';', encoding='ISO-8859-1')
 
-df = pd.read_csv('data/dataset.csv', delimiter=';', encoding='ISO-8859-1')
-
+if __name__ == "__main__":
+    function()
