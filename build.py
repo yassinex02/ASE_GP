79aa75b20299d6ad316255ce40050498ac5eb979
yassinex02 <tahayassinemoumni@gmail.com>
2023-09-19 22:30:49 +0200

    adding requiremnts.txt file

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def function():
     df = pd.read_csv('data/dataset.csv', delimiter=';', encoding='ISO-8859-1')
+
 
 if __name__ == "__main__":
     function()
