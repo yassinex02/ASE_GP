0d5bf7519d9c88173f77f0a5407cf350817d60b1
Momo <mozeart@hotmail.com>
2023-09-23 22:26:04 +0200

    fixed an error with build.py

--- build.py
+++ build.py
@@ -1,12 +1,15 @@
+#%%
 import pandas as pd
 from src.functions import get_nof_squirrels_bycolors,plot_white_squirrels
 
 
 def plot():
-    df = pd.read_csv('data/dataset.csv', delimiter=';', encoding='ISO-8859-1')
+    df = pd.read_csv('data/dataset.csv', encoding='ISO-8859-1')
     get_nof_squirrels_bycolors(df)
     plot_white_squirrels(df)
 
 
 if __name__ == "__main__":
     plot()
+
+# %%
