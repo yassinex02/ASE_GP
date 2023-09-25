0c612abd29d2be0ca00f604be689bf7a785c0452
yassinex02 <tahayassinemoumni@gmail.com>
2023-09-25 16:02:57 +0200

    'try index.html'

--- build.py
+++ build.py
@@ -1,10 +1,9 @@
-#%%
 import pandas as pd
-from src.functions import get_nof_squirrels_bycolors,plot_white_squirrels
+from src.functions import get_nof_squirrels_bycolors, plot_white_squirrels
 
 
 def plot():
-    df = pd.read_csv('data/dataset.csv', encoding='ISO-8859-1')
+    df = pd.read_csv('data/dataset.csv', delimiter=';', encoding='ISO-8859-1')
     get_nof_squirrels_bycolors(df)
     plot_white_squirrels(df)
 
