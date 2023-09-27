fa09c08a87e2da65f38a54b219b88b77ecce8073
yassinex02 <tahayassinemoumni@gmail.com>
2023-09-27 07:30:30 +0200

    testing if a change in code will be seen in pages

--- src/functions.py
+++ src/functions.py
@@ -8,7 +8,7 @@ def get_nof_squirrels_bycolors(df):
     plt.figure(figsize=(10, 6))
     fur_color_counts.plot(kind='bar', color='skyblue')
     plt.title('Number of Squirrels Per Fur Color')
-    plt.xlabel('Fur Color')
+    # plt.xlabel('Fur Color')
     plt.ylabel('Number of Squirrels')
     plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
     plt.tight_layout()
@@ -19,23 +19,22 @@ def get_nof_squirrels_bycolors(df):
     plt.show()
 
 
-
 # Map of White Squirrels: Create a map that marks the locations of white squirrels spotted during the census.
 
 def plot_white_squirrels(df):
 
     white_squirrels = df[df["Highlights in Fur Color"] == "White"]
 
-    m = folium.Map(location=[white_squirrels["Squirrel Latitude (DD.DDDDDD)"].mean(), 
-                             white_squirrels["Squirrel Longitude (-DD.DDDDDD)"].mean()], 
+    m = folium.Map(location=[white_squirrels["Squirrel Latitude (DD.DDDDDD)"].mean(),
+                             white_squirrels["Squirrel Longitude (-DD.DDDDDD)"].mean()],
                    zoom_start=13)
 
     # Add markers for each white squirrel sighting
     for index, row in white_squirrels.iterrows():
         folium.Marker([row["Squirrel Latitude (DD.DDDDDD)"], row["Squirrel Longitude (-DD.DDDDDD)"]],
                       tooltip=row["Park Name"]).add_to(m)
-        
-    mapfile= "map.html"
+
+    mapfile = "map.html"
     m.save(mapfile)
     webbrowser.open(mapfile)
-    return 
+    return
